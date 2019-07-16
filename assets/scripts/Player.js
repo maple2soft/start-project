e27b2e8a364b8c3aa678ec5fdb3d0446ed9b373d
maple2soft <wyjufe@qq.com>
2019-07-16 16:22:03 +0800

    20190716 code

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -19,7 +19,7 @@ cc.Class({
         // 最大移动速度
         maxMoveSpeed: 0,
         // 加速度
-        accel: 0,
+        accel: 0,       
     },
 
     setJumpAction: function () {
@@ -31,17 +31,92 @@ cc.Class({
         return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
     },
 
+    onKeyDown(event) {
+        // set a acc flag
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = true;
+                break;
+            case cc.macro.KEY.left:
+                this.accLeft = true;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = true;
+                break;
+            case cc.macro.KEY.right:
+                this.accRight = true;
+                break;
+        }
+    },
+
+    onKeyUp(event) {
+        // set a acc flag
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.left:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = false;
+                break;
+            case cc.macro.KEY.right:
+                this.accRight = false;
+                break;
+        }
+    },
+
     // LIFE-CYCLE CALLBACKS:
 
     onLoad: function () {
         // 初始化跳跃动作
         this.jumpAction = this.setJumpAction();
         this.node.runAction(this.jumpAction);
+
+        // init accLeft and accRight
+        this.accLeft = false;
+        this.accRight = false;
+
+        // set xSpeed
+        this.xSpeed = 0;
+
+        // add keyboard listener
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+
+    onDestroy() {
+        // 取消键盘监听事件
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.systemEvent.EventType.KEY_UP, this.onKeyUp, this);
     },
 
     start() {
 
     },
 
-    // update (dt) {},
+    update(dt) {
+        // update xSpeed
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+
+        // limit xSpeed to maxMovSpeed
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // update player x position
+        this.node.x += this.xSpeed * dt;        
+
+        // confine the monster in screen
+        if (this.node.x >= 441) {
+            this.node.x = 441;
+        } else if (this.node.x <= -441) {
+            this.node.x = -441;
+        }
+    },
 });
---
+++ assets/scripts/Star.js
@@ -0,0 +1,28 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // 星星和主角之间的距离小于这个数值的时候，就会完成收集
+        pickRadius: 0,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
